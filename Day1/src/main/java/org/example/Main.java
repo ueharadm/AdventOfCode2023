4a512131bd0ef7af84d3f2b869810a0268f15f97
victor.uehara <victor.uehara@sankhya.com.br>
2023-12-12 23:00:00 -0300

    Day1 part2 regex try

--- Day1/src/main/java/org/example/Main.java
+++ Day1/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,41 +16,62 @@ public class Main {
             String line;
             int sum = 0;
 
+            // Create a map to map spelled-out numbers to their corresponding numerical values
+            Map<String, Integer> spelledOutNumbers = new HashMap<>();
+            spelledOutNumbers.put("one", 1);
+            spelledOutNumbers.put("two", 2);
+            spelledOutNumbers.put("three", 3);
+            spelledOutNumbers.put("four", 4);
+            spelledOutNumbers.put("five", 5);
+            spelledOutNumbers.put("six", 6);
+            spelledOutNumbers.put("seven", 7);
+            spelledOutNumbers.put("eight", 8);
+            spelledOutNumbers.put("nine", 9);
+
             // Read each line until the end of the file
             while ((line = reader.readLine()) != null) {
                 // Process each line as needed
-                System.out.println(line);
+                System.out.println("#### "+line);
                 try{
                     // Define the pattern to match a number
-                    Pattern pattern = Pattern.compile("\\d");
+                    //Pattern pattern = Pattern.compile("\\d");
+                    Pattern pattern = Pattern.compile("(one|two|three|four|five|six|seven|eight|nine|\\d)(?!\\d)");
+
+
 
                     // Create a matcher for the input string
                     Matcher matcher = pattern.matcher(line);
+
 
                     // Find the first match
                     if (matcher.find()) {
                         // Extract and print the first matched number
                         String firstNumber = matcher.group();
+
+                        String numericalFirstNumber = spelledOutNumbers.containsKey(firstNumber) ? ""+spelledOutNumbers.get(firstNumber) : firstNumber;
+
                         String lastNumber = firstNumber;
-                        System.out.println("First number: " + firstNumber);
+                        System.out.println("#### "+"First number: " + numericalFirstNumber);
 
                         // Find the last match
                         while (matcher.find()) {
                             lastNumber = matcher.group();
                         }
-                        System.out.println("Last number: " + lastNumber);
+                        String numericalLastNumber = spelledOutNumbers.containsKey(lastNumber) ? ""+spelledOutNumbers.get(lastNumber) : lastNumber;
+                        System.out.println("#### "+"Last number: " + numericalLastNumber);
 
-                        int finalNumber = Integer.parseInt(""+firstNumber+lastNumber);
-                        System.out.println("FinalNumber = " + finalNumber);
+                        int finalNumber = Integer.parseInt(""+numericalFirstNumber+numericalLastNumber);
+                        System.out.println("#### "+"FinalNumber = " + finalNumber);
+                        System.out.println("");
+                        System.out.println("");
                         sum+=finalNumber;
-                    } else {
-                        System.out.println("No number found in the string.");
-                    }}
+                    }
+                }
                 catch (Exception e){
                     System.out.println("Error: " + e.getMessage());
                 }
             }
-            System.out.println("Total sum: " + sum);
+            System.out.println("#### "+"Total sum: " + sum);
         } catch (IOException e) {
             e.printStackTrace(); // Handle potential IOException
         }
